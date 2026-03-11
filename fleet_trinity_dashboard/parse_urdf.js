fc24d136af6d22c7afab4f630d86ac7bbe526abc
Frank van Laarhoven <frankleroyvan@gmail.com>
2026-03-11 05:54:18 +0000

    feat(ml): deploy OpenVLA saferpath_hybrid_train.py background process and fix URDFLoader Nextjs SSR crash

---
+++ fleet_trinity_dashboard/parse_urdf.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const URDFLoader = require('urdf-loader');
+const { DOMParser } = require('xmldom');
+
+const loader = new URDFLoader();
+loader.parseOptions.parseDOM = (content) => new DOMParser().parseFromString(content, 'text/xml');
+
+try {
+  const content = fs.readFileSync('public/robots/unitree_g1/g1_29dof.urdf', 'utf8');
+  const robot = loader.parse(content);
+  console.log('Successfully parsed URDF. Object keys:', Object.keys(robot));
+} catch (e) {
+  console.error('Failed to parse URDF:', e);
+}
